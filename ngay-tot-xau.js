ed6e28f4f87b78a104a9c27553b3b63ac2b88f49
windchipi <39388155+windchipi@users.noreply.github.com>
2026-04-15 09:13:30 +0700

    Create ngay-tot-xau.js

---
+++ ngay-tot-xau.js
@@ -0,0 +1,27 @@
+// Database Xem Ngày - Fortune Toolbox
+const DateJobDB = [
+    { 
+        id: "nghi-viec", 
+        name: "Nghỉ việc / Viết đơn", 
+        good: "Ngày cực tốt để giải phóng linh hồn. Sếp có thể sẽ khóc nhưng bạn sẽ cười.",
+        bad: "Ngày này nghỉ dễ bị quỵt lương hoặc bị HR giữ sổ bảo hiểm lâu. Đợi đi!" 
+    },
+    { 
+        id: "xin-viec", 
+        name: "Gửi CV / Phỏng vấn", 
+        good: "Vận khí đang lên, nói gì cũng thấy thuyết phục. CV của bạn lấp lánh như vàng.",
+        bad: "Ngày này dễ gặp nhà tuyển dụng hắc ám hoặc quên lưu file CV. Bình tĩnh!" 
+    },
+    { 
+        id: "an-uong", 
+        name: "Đi ăn với bạn bè", 
+        good: "Ăn gì cũng ngon, không lo đau bụng, lại còn dễ được bạn bao.",
+        bad: "Dễ gặp quán đông, phục vụ chậm hoặc đến lúc tính tiền thì... quên ví." 
+    },
+    { 
+        id: "code-game", 
+        name: "Code tính năng mới", 
+        good: "Logic thông suốt, không một dòng bug. Compile một phát ăn ngay.",
+        bad: "Bug đẻ thêm bug. Hôm nay chỉ nên ngồi chơi, đừng động vào source code." 
+    }
+];
